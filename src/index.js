9a6f74e51c9dd78ae40db782729296687b43caf2
Jeremy Thomerson <jeremy@thomersonfamily.com>
2018-04-12 23:51:56 -0400

    Refs #7: Use new CloudFormation support for Lambda@Edge
    Rather than allowing the CloudFront distribution to be deployed by CloudFormation and then
    updating the distribution manually ourselves to add the Lambda association (as was done in
    v1 of this plugin), we can now use the CloudFormation built-in support for Lambda@Edge.
    
    The plugin is still needed because CloudFront (via CloudFormation) needs a specific
    _version_ of a Lambda function. Thus, in our CloudFormation template (a.k.a.
    serverless.yml in this context), we would need to add the function associations to our
    distribution by adding `{ Ref: 'TheFunctionVersionLogicalID' }`. Unfortunately, though,
    the function version is named "randomly" by Serverless (actually, the logical name is
    based on the SHA checksum of the code zip), so we can't compute the value ourselves in the
    template.
    
    Thus, the plugin reads our event triggers from the serverless.yml function configuration
    and then programmatically updates the CloudFormation template to add the Lambda function
    associations to the versions of the function that are being deployed.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var _ = require('underscore'),
-    Q = require('q'),
     Class = require('class.extend'),
     VALID_EVENT_TYPES = [ 'viewer-request', 'origin-request', 'viewer-response', 'origin-response' ];
 
@@ -17,53 +16,16 @@ module.exports = Class.extend({
       }
 
       this.hooks = {
-         'aws:package:finalize:mergeCustomProviderResources': this._onPackageCustomResources.bind(this),
-         'before:deploy:finalize': this._onBeforeDeployFinalize.bind(this),
-         'package:function:package': this._onFunctionPackage.bind(this),
+         'aws:package:finalize:mergeCustomProviderResources': this._modifyTemplate.bind(this),
       };
 
-   },
-
-   _onFunctionPackage: function() {
-      this._serverless.cli.log('Function being packaged ...');
-   },
-
-   _onPackageCustomResources: function() {
-      this._modifyTemplate();
-   },
-
-   _onBeforeDeployFinalize: function() {
-      var cnt = this._pendingAssociations.length;
-
-      if (cnt === 0) {
-         return;
-      }
-
-      /**
-       * Each entry in the this._pendingAssociations array looks like this:
-       * {
-       *    "fnLogicalName": "YourFnNameLambdaFunction",
-       *    "distLogicalName": "WebsiteDistribution",
-       *    "fnCurrentVersionOutputName": "YourFnNameLambdaFunctionQualifiedArn",
-       *    "eventType": "origin-request",
-       * }
-       */
-
-      this._serverless.cli.log(
-         'Checking to see if ' + cnt +
-         (cnt > 1 ? ' functions need ' : ' function needs ') +
-         'to be associated to CloudFront'
-      );
-
-      return Q.all([ this._getFunctionsToAssociate(), this._getDistributionPhysicalIDs() ])
-         .spread(this._updateDistributionsAsNecessary.bind(this));
    },
 
    _modifyTemplate: function() {
       var template = this._serverless.service.provider.compiledCloudFormationTemplate;
 
       this._modifyExecutionRole(template);
-      this._modifyLambdaFunctions(this._serverless.service.functions, template);
+      this._modifyLambdaFunctionsAndDistributions(this._serverless.service.functions, template);
    },
 
    _modifyExecutionRole: function(template) {
@@ -109,53 +71,47 @@ module.exports = Class.extend({
       }
    },
 
-   _modifyLambdaFunctions: function(functions, template) {
+   _modifyLambdaFunctionsAndDistributions: function(functions, template) {
       var self = this;
 
-      this._pendingAssociations = _.chain(functions)
-         .reduce(function(memo, fnDef, fnName) {
-            var distName, evtType, dist;
+      _.chain(functions)
+         .pick(_.property('lambdaAtEdge')) // `pick` is used like `filter`, but for objects
+         .each(function(fnDef, fnName) {
+            var outputName = self._provider.naming.getLambdaVersionOutputLogicalId(fnName),
+                fnLogicalName = self._provider.naming.getLambdaLogicalId(fnName),
+                distName = fnDef.lambdaAtEdge.distribution,
+                fnProps = template.Resources[fnLogicalName].Properties,
+                evtType = fnDef.lambdaAtEdge.eventType,
+                output = template.Outputs[outputName],
+                dist = template.Resources[distName],
+                distConfig, fnAssociations, versionLogicalID;
 
-            if (!fnDef.lambdaAtEdge) {
-               return memo;
+            if (!_.contains(VALID_EVENT_TYPES, evtType)) {
+               throw new Error('"' + evtType + '" is not a valid event type, must be one of: ' + VALID_EVENT_TYPES.join(', '));
             }
 
-            distName = fnDef.lambdaAtEdge.distribution;
-            evtType = fnDef.lambdaAtEdge.eventType;
-            dist = template.Resources[distName];
-
-            if (fnDef.lambdaAtEdge) {
-               if (!_.contains(VALID_EVENT_TYPES, evtType)) {
-                  throw new Error('"' + evtType + '" is not a valid event type, must be one of: ' + VALID_EVENT_TYPES.join(', '));
-               }
-
-               if (!dist) {
-                  throw new Error('Could not find resource with logical name "' + distName + '"');
-               }
-
-               if (dist.Type !== 'AWS::CloudFront::Distribution') {
-                  throw new Error('Resource with logical name "' + distName + '" is not type AWS::CloudFront::Distribution');
-               }
-
-               memo.push({
-                  fnLogicalName: self._provider.naming.getLambdaLogicalId(fnName),
-                  distLogicalName: fnDef.lambdaAtEdge.distribution,
-                  fnCurrentVersionOutputName: self._provider.naming.getLambdaVersionOutputLogicalId(fnName),
-                  eventType: evtType,
-               });
+            if (!dist) {
+               throw new Error('Could not find resource with logical name "' + distName + '"');
             }
 
-            return memo;
-         }, [])
-         .each(function(fn) {
-            var fnProps = template.Resources[fn.fnLogicalName].Properties;
+            if (dist.Type !== 'AWS::CloudFront::Distribution') {
+               throw new Error('Resource with logical name "' + distName + '" is not type AWS::CloudFront::Distribution');
+            }
+
+            distConfig = dist.Properties.DistributionConfig;
+            fnAssociations = distConfig.DefaultCacheBehavior.LambdaFunctionAssociations;
+            versionLogicalID = (output ? output.Value.Ref : null);
+
+            if (!versionLogicalID) {
+               throw new Error('Could not find output by name of "' + outputName + '" or value from it to use version ARN');
+            }
 
             if (fnProps && fnProps.Environment && fnProps.Environment.Variables) {
                self._serverless.cli.log(
                   'Removing ' +
                   _.size(fnProps.Environment.Variables) +
                   ' environment variables from function "' +
-                  fn.fnLogicalName +
+                  fnLogicalName +
                   '" because Lambda@Edge does not support environment variables'
                );
 
@@ -165,163 +121,21 @@ module.exports = Class.extend({
                   delete fnProps.Environment;
                }
             }
-         })
-         .value();
-   },
 
-   _updateDistributionsAsNecessary: function(fns, dists) {
-      return Q.all(_.map(dists, this._updateDistributionAsNecessary.bind(this, fns)));
-   },
-
-   _waitForDistributionDeployed: function(distPhysicalID, distLogicalName) {
-      var self = this,
-          cloudfront = new this._provider.sdk.CloudFront(this._provider.getCredentials()),
-          firstDot = true,
-          running = true;
-
-      function dotPrinter() {
-         if (running) {
-            if (firstDot) {
-               self._serverless.cli.log('Waiting for CloudFront distribution "' + distLogicalName + '" to be deployed');
-               firstDot = false;
+            if (!_.isArray(fnAssociations)) {
+               fnAssociations = distConfig.DefaultCacheBehavior.LambdaFunctionAssociations = [];
             }
-            self._serverless.cli.printDot();
-            setTimeout(dotPrinter, 2000);
-         }
-      }
 
-      setTimeout(dotPrinter, 1000);
-
-      return Q.ninvoke(cloudfront, 'waitFor', 'distributionDeployed', { Id: distPhysicalID })
-         .then(function(resp) {
-            running = false;
-            if (!firstDot) {
-               // we have printed a dot, so clear the line
-               this._serverless.cli.consoleLog('');
-            }
-            this._serverless.cli.log('Distribution "' + distLogicalName + '" is now in "' + resp.Distribution.Status + '" state');
-         }.bind(this));
-   },
-
-   _updateDistributionAsNecessary: function(fns, distID, distName) {
-      var self = this;
-
-      return this._waitForDistributionDeployed(distID, distName)
-         .then(function() {
-            return self._provider.request('CloudFront', 'getDistribution', { Id: distID });
-         })
-         .then(function(resp) {
-            var config = resp.Distribution.DistributionConfig,
-                changed = self._modifyDistributionConfigIfNeeded(config, fns[distName]),
-                updateParams = { Id: distID, DistributionConfig: config, IfMatch: resp.ETag };
-
-            if (changed) {
-               self._serverless.cli.log('Updating distribution "' + distName + '" because we updated Lambda@Edge associations on it');
-               return self._provider.request('CloudFront', 'updateDistribution', updateParams)
-                  .then(function() {
-                     return self._waitForDistributionDeployed(distID, distName);
-                  })
-                  .then(function() {
-                     self._serverless.cli.log('Done updating distribution "' + distName + '"');
-                  });
-            }
+            fnAssociations.push({
+               EventType: evtType,
+               LambdaFunctionARN: { Ref: versionLogicalID },
+            });
 
             self._serverless.cli.log(
-               'The distribution is already configured with the current versions of each Lambda@Edge function it needs'
+               'Added "' + evtType + '" Lambda@Edge association for version "' +
+               versionLogicalID + '" to distribution "' + distName + '"'
             );
          });
    },
-
-   _modifyDistributionConfigIfNeeded: function(distConfig, fns) {
-      var changed = this._associateFunctionsToBehavior(distConfig.DefaultCacheBehavior, fns);
-
-      _.each(distConfig.CacheBehaviors.Items, function(beh) {
-         var behaviorChanged = this._associateFunctionsToBehavior(beh, fns);
-
-         changed = changed || behaviorChanged;
-      }.bind(this));
-
-      return changed;
-   },
-
-   _associateFunctionsToBehavior: function(beh, fns) {
-      var changed = false;
-
-      _.each(fns, function(fn) {
-         var existing = _.findWhere(beh.LambdaFunctionAssociations.Items, { EventType: fn.eventType });
-
-         if (!existing) {
-            this._serverless.cli.log('Adding new Lamba@Edge association for ' + fn.eventType + ': ' + fn.fnARN);
-            beh.LambdaFunctionAssociations.Items.push({
-               EventType: fn.eventType,
-               LambdaFunctionARN: fn.fnARN,
-            });
-            changed = true;
-         } else if (existing.LambdaFunctionARN !== fn.fnARN) {
-            this._serverless.cli.log('Updating ' + fn.eventType + ' to use ' + fn.fnARN + ' (was ' + existing.LambdaFunctionARN + ')');
-            existing.LambdaFunctionARN = fn.fnARN;
-            changed = true;
-         }
-      }.bind(this));
-
-      if (changed) {
-         beh.LambdaFunctionAssociations.Quantity = beh.LambdaFunctionAssociations.Items.length;
-      }
-
-      return changed;
-   },
-
-   _getFunctionsToAssociate: function() {
-      var stackName = this._provider.naming.getStackName();
-
-      return this._provider.request('CloudFormation', 'describeStacks', { StackName: stackName })
-         .then(function(resp) {
-            var stack = _.findWhere(resp.Stacks, { StackName: stackName });
-
-            if (!stack) {
-               throw new Error('CloudFormation did not return a stack with name "' + stackName + '"');
-            }
-
-            return _.reduce(this._pendingAssociations, function(memo, pending) {
-               var outputName = pending.fnCurrentVersionOutputName,
-                   output = _.findWhere(stack.Outputs, { OutputKey: outputName });
-
-               if (!output) {
-                  throw new Error('Stack "' + stackName + '" did not have an output with name "' + outputName + '"');
-               }
-
-               if (!memo[pending.distLogicalName]) {
-                  memo[pending.distLogicalName] = [];
-               }
-
-               memo[pending.distLogicalName].push({
-                  eventType: pending.eventType,
-                  fnARN: output.OutputValue,
-               });
-
-               return memo;
-            }, {});
-         }.bind(this));
-   },
-
-   _getDistributionPhysicalIDs: function() {
-      var stackName = this._provider.naming.getStackName();
-
-      return this._provider.request('CloudFormation', 'describeStackResources', { StackName: stackName })
-         .then(function(resp) {
-            return _.reduce(this._pendingAssociations, function(memo, pending) {
-               var resource = _.findWhere(resp.StackResources, { LogicalResourceId: pending.distLogicalName });
-
-               if (!resource) {
-                  throw new Error('Stack "' + stackName + '" did not have a resource with logical name "' + pending.distLogicalName + '"');
-               }
-
-               memo[pending.distLogicalName] = resource.PhysicalResourceId;
-
-               return memo;
-            }, {});
-         }.bind(this));
-   },
-
 
 });
